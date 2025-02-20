539e74032fa2f623e4fc7694abb6c5643b1d45f3
Akhilesh Kushwaha <115693265+mr-akhi2@users.noreply.github.com>
2025-02-20 13:49:26 +0530

    Update cloudinary.js

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = v2;
 require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: "dyanneafd",
+  api_key:"913954779263274",
+  api_secret: "D3TDQK1duS56L9usJ0UM4gyW2bA",
 });
 
 module.exports = cloudinary;
